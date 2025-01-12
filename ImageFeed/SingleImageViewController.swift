f4e670567729dbb8355cf0dada06c673b889a290
IceRedline <scpd2604@gmail.com>
2025-01-12 20:41:35 +0300

    Добавлен функционал зума с центрированием и возможность поделиться изображеним

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -10,20 +10,71 @@ import UIKit
 class SingleImageViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var image: UIImage? {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            if let image {
+                rescaleAndCenterImageInScrollView(image: image)
+            }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        imageView.frame.size = image?.size ?? CGSize(width: 0, height: 0)
+        if let image {
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
     }
-
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    
+    @IBAction func shareButtonTapped() {
+        if let image {
+            let avc = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            present(avc, animated: true)
+        }
+    }
+    
+    
     @IBAction func backButtonTapped() {
         dismiss(animated: true)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let offsetX = max((scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5, 0.0)
+        let offsetY = max((scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5, 0.0)
+
+        scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: offsetY, right: offsetX);
     }
 }
